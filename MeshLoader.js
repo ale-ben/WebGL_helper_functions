2bfcdd9a1bfde39618958b2057c7ca493110ff20
Alessandro Benetton <aleben98@gmail.com>
2022-12-22 18:13:16 +0100

    [master] Moved obj parsing logic to MeshLoader

--- MeshLoader.js
+++ MeshLoader.js
@@ -13,7 +13,7 @@ export class MeshLoader {
 	 * @param {string} text - The OBJ file content
 	 * @returns {object} - An object containing the various geometries and obj defined materials
 	 */
-	static parseOBJ(text) {
+	static ParseOBJ(text) {
 		// Since the internal indices start from 1 let's just fill in the 0th data
 		const objPositions = [[0, 0, 0]];
 		const objTexcoords = [[0, 0]];
@@ -227,7 +227,7 @@ export class MeshLoader {
 	 * @param {string} text - The MTL file content
 	 * @returns {object} - The materials definitions
 	 */
-	static parseMTL(text) {
+	static ParseMTL(text) {
 		/**
 		 * Object containing all the materials with the material name as a keyword
 		 */
@@ -276,110 +276,231 @@ export class MeshLoader {
 	}
 
 	/**
-	 * Create and binf a base texture
-	 * @param {*} gl The webgl environment
-	 * @param {*} pixel Array with color values
-	 * @returns 
+	 * Load an obj and mesh in the passed object.
+	 * The passed object MUST contain the following properties:
+	 * - name: name of the object
+	 * - filePath: path to the obj file
+	 * 
+	 * @param {*} gl 
+	 * @param {*} object 
 	 */
-	static create1PixelTexture(gl, pixel) {
-		const texture = gl.createTexture();
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-			new Uint8Array(pixel));
-		return texture;
-	}
+	static async LoadOBJAndMesh(gl, object) {
+		if (debug && debug == true) console.log("Loading mesh " + object.name + " from " + object.filePath + (object.mtlPath ? " with MTL file " + object.mtlPath : ""));
+		// Load OBJ file
+		const objResponse = await fetch(object.filePath);
+		const objText = await objResponse.text();
+		const obj = this.ParseOBJ(objText);
 
-	/**
-	 * Create a texture from an image
-	 * @param {*} gl The webgl environment
-	 * @param {*} url Address of the image
-	 * @returns 
-	 */
-	static createTexture(gl, url) {
+		// Load MTL file
+		const baseHref = new URL(object.filePath, window.location.href);
+		let materials;
+		if (!object.mtlPath) {
+			const matTexts = await Promise.all(obj.materialLibs.map(async filename => {
+				const matHref = new URL(filename, baseHref).href;
+				const response = await fetch(matHref);
+				return await response.text();
+			}));
+			materials = this.ParseMTL(matTexts.join('\n'));
+		} else {
+			if (debug && debug == true) console.log("Loading manually defined MTL file " + object.mtlPath);
+			const mtlResponse = await fetch(object.mtlPath);
+			const mtlText = await mtlResponse.text();
+			materials = this.ParseMTL(mtlText);
+		}
 
-		const isPowerOf2 = (value) => (value & (value - 1)) === 0;
 
-		const texture = MeshLoader.create1PixelTexture(gl, [128, 192, 255, 255]);
-		// Asynchronously load an image
-		const image = new Image();
-		image.src = url;
-		image.addEventListener('load', function () {
-			// Now that the image has loaded make copy it to the texture.
-			gl.bindTexture(gl.TEXTURE_2D, texture);
-			gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+		const textures = {
+			defaultWhite: create1PixelTexture(gl, [255, 255, 255, 255]),
+			defaultNormal: create1PixelTexture(gl, [127, 127, 255, 0]),
+		};
 
-			// Check if the image is a power of 2 in both dimensions.
-			if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-				// Yes, it's a power of 2. Generate mips.
-				gl.generateMipmap(gl.TEXTURE_2D);
+		const defaultMaterial = {
+			diffuse: [1, 1, 1],
+			diffuseMap: textures.defaultWhite,
+			normalMap: textures.defaultNormal,
+			ambient: [0, 0, 0],
+			specular: [1, 1, 1],
+			specularMap: textures.defaultWhite,
+			shininess: 400,
+			opacity: 1,
+		};
+
+		// load texture for materials
+		for (const material of Object.values(materials)) {
+			Object.entries(material)
+				.filter(([key]) => key.endsWith('Map'))
+				.forEach(([key, filename]) => {
+					let texture = textures[filename];
+					if (!texture) {
+						const textureHref = new URL(filename, baseHref).href;
+						texture = createTexture(gl, textureHref);
+						textures[filename] = texture;
+					}
+					material[key] = texture;
+				});
+		}
+
+		object.parts = obj.geometries.map(({ material, data }) => { // Since each geometry has it's own buffer, we have to load them separately
+			// Because data is just named arrays like this
+			//
+			// {
+			//   position: [...],
+			//   texcoord: [...],
+			//   normal: [...],
+			// }
+			//
+			// and because those names match the attributes in our vertex
+			// shader we can pass it directly into `createBufferInfoFromArrays`
+			// from the article "less code more fun".
+
+			if (data.color) {
+				if (data.position.length === data.color.length) {
+					// it's 3. The our helper library assumes 4 so we need
+					// to tell it there are only 3.
+					data.color = { numComponents: 3, data: data.color };
+				}
 			} else {
-				// No, it's not a power of 2. Turn of mips and set wrapping to clamp to edge
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+				// there are no vertex colors so just use constant white
+				data.color = { value: [1, 1, 1, 1] };
 			}
+
+			// generate tangents if we have the data to do so.
+			if (data.texcoord && data.normal) {
+				data.tangent = generateTangents(data.position, data.texcoord);
+			} else {
+				// There are no tangents
+				data.tangent = { value: [1, 0, 0] };
+			}
+
+			if (!data.texcoord) {
+				data.texcoord = { value: [0, 0] };
+			}
+
+			if (!data.normal) {
+				// we probably want to generate normals if there are none
+				data.normal = { value: [0, 0, 1] };
+			}
+
+			// create a buffer for each array by calling
+			// gl.createBuffer, gl.bindBuffer, gl.bufferData
+			const bufferInfo = webglUtils.createBufferInfoFromArrays(gl, data);
+			return {
+				material: {
+					...defaultMaterial,
+					...materials[material],
+				},
+				bufferInfo,
+			};
 		});
-		return texture;
+
+		if (debug && debug == true) console.log("Loaded mesh for " + object.name + ". ", object);
+	}
+}
+
+/**
+ * Create and binf a base texture
+ * @param {*} gl The webgl environment
+ * @param {*} pixel Array with color values
+ * @returns 
+ */
+function create1PixelTexture(gl, pixel) {
+	const texture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+		new Uint8Array(pixel));
+	return texture;
+}
+
+/**
+ * Create a texture from an image
+ * @param {*} gl The webgl environment
+ * @param {*} url Address of the image
+ * @returns 
+ */
+function createTexture(gl, url) {
+
+	const isPowerOf2 = (value) => (value & (value - 1)) === 0;
+
+	const texture = create1PixelTexture(gl, [128, 192, 255, 255]);
+	// Asynchronously load an image
+	const image = new Image();
+	image.src = url;
+	image.addEventListener('load', function () {
+		// Now that the image has loaded make copy it to the texture.
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+		// Check if the image is a power of 2 in both dimensions.
+		if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+			// Yes, it's a power of 2. Generate mips.
+			gl.generateMipmap(gl.TEXTURE_2D);
+		} else {
+			// No, it's not a power of 2. Turn of mips and set wrapping to clamp to edge
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		}
+	});
+	return texture;
+}
+
+function generateTangents(position, texcoord, indices) {
+
+	function makeIndexIterator(indices) {
+		let ndx = 0;
+		const fn = () => indices[ndx++];
+		fn.reset = () => { ndx = 0; };
+		fn.numElements = indices.length;
+		return fn;
 	}
 
-	static generateTangents(position, texcoord, indices) {
-
-		function makeIndexIterator(indices) {
-			let ndx = 0;
-			const fn = () => indices[ndx++];
-			fn.reset = () => { ndx = 0; };
-			fn.numElements = indices.length;
-			return fn;
-		}
-
-		function makeUnindexedIterator(positions) {
-			let ndx = 0;
-			const fn = () => ndx++;
-			fn.reset = () => { ndx = 0; };
-			fn.numElements = positions.length / 3;
-			return fn;
-		}
-
-		const subtractVector2 = (a, b) => a.map((v, ndx) => v - b[ndx]);
-
-		const getNextIndex = indices ? makeIndexIterator(indices) : makeUnindexedIterator(position);
-		const numFaceVerts = getNextIndex.numElements;
-		const numFaces = numFaceVerts / 3;
-
-		const tangents = [];
-		for (let i = 0; i < numFaces; ++i) {
-			const n1 = getNextIndex();
-			const n2 = getNextIndex();
-			const n3 = getNextIndex();
-
-			const p1 = position.slice(n1 * 3, n1 * 3 + 3);
-			const p2 = position.slice(n2 * 3, n2 * 3 + 3);
-			const p3 = position.slice(n3 * 3, n3 * 3 + 3);
-
-			const uv1 = texcoord.slice(n1 * 2, n1 * 2 + 2);
-			const uv2 = texcoord.slice(n2 * 2, n2 * 2 + 2);
-			const uv3 = texcoord.slice(n3 * 2, n3 * 2 + 2);
-
-			const dp12 = m4.subtractVectors(p2, p1);
-			const dp13 = m4.subtractVectors(p3, p1);
-
-			const duv12 = subtractVector2(uv2, uv1);
-			const duv13 = subtractVector2(uv3, uv1);
-
-			const f = 1.0 / (duv12[0] * duv13[1] - duv13[0] * duv12[1]);
-			const tangent = Number.isFinite(f)
-				? m4.normalize(m4.scaleVector(m4.subtractVectors(
-					m4.scaleVector(dp12, duv13[1]),
-					m4.scaleVector(dp13, duv12[1]),
-				), f))
-				: [1, 0, 0];
-
-			tangents.push(...tangent, ...tangent, ...tangent);
-		}
-
-		return tangents;
+	function makeUnindexedIterator(positions) {
+		let ndx = 0;
+		const fn = () => ndx++;
+		fn.reset = () => { ndx = 0; };
+		fn.numElements = positions.length / 3;
+		return fn;
 	}
+
+	const subtractVector2 = (a, b) => a.map((v, ndx) => v - b[ndx]);
+
+	const getNextIndex = indices ? makeIndexIterator(indices) : makeUnindexedIterator(position);
+	const numFaceVerts = getNextIndex.numElements;
+	const numFaces = numFaceVerts / 3;
+
+	const tangents = [];
+	for (let i = 0; i < numFaces; ++i) {
+		const n1 = getNextIndex();
+		const n2 = getNextIndex();
+		const n3 = getNextIndex();
+
+		const p1 = position.slice(n1 * 3, n1 * 3 + 3);
+		const p2 = position.slice(n2 * 3, n2 * 3 + 3);
+		const p3 = position.slice(n3 * 3, n3 * 3 + 3);
+
+		const uv1 = texcoord.slice(n1 * 2, n1 * 2 + 2);
+		const uv2 = texcoord.slice(n2 * 2, n2 * 2 + 2);
+		const uv3 = texcoord.slice(n3 * 2, n3 * 2 + 2);
+
+		const dp12 = m4.subtractVectors(p2, p1);
+		const dp13 = m4.subtractVectors(p3, p1);
+
+		const duv12 = subtractVector2(uv2, uv1);
+		const duv13 = subtractVector2(uv3, uv1);
+
+		const f = 1.0 / (duv12[0] * duv13[1] - duv13[0] * duv12[1]);
+		const tangent = Number.isFinite(f)
+			? m4.normalize(m4.scaleVector(m4.subtractVectors(
+				m4.scaleVector(dp12, duv13[1]),
+				m4.scaleVector(dp13, duv12[1]),
+			), f))
+			: [1, 0, 0];
+
+		tangents.push(...tangent, ...tangent, ...tangent);
+	}
+
+	return tangents;
 }
 
 /**
