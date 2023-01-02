f492c1dcdb1425ccad3351dc348d409340e26bf8
Alessandro Benetton <aleben98@gmail.com>
2023-01-02 11:55:01 +0100

    [M3D-7-detect-click-on-object] Split renderer in drawObject and computeObjWorld

--- RenderEngine.js
+++ RenderEngine.js
@@ -13,52 +13,59 @@ export class RenderEngine {
      * @param {*} objList Array of objects to render. Each object can have a center object and a rotation object but must have a parts array.
      */
     render(cameraUniforms, programInfo, objList) {
-        this.gl.useProgram(programInfo.program);
-
-        webglUtils.setUniforms(programInfo, cameraUniforms);
-
         objList.forEach(obj => {
-            // compute the world matrix
-            // are at the same space.
-            let u_world = m4.identity();
-
-            // Handle object translation
-            if (obj.center && (obj.center.x != 0 || obj.center.y != 0 || obj.center.z != 0)) {
-                u_world = m4.translate(u_world, obj.center.x, obj.center.y, obj.center.z);
-            }
-
-            // Handle object rotation
-            //u_world = m4.xRotate(u_world, time);
-            //u_world = m4.yRotate(u_world, time);
-            //u_world = m4.zRotate(u_world, time);
-
-            if (obj.rotation) {
-                if (obj.rotation.x != 0) {
-                    u_world = m4.xRotate(u_world, obj.rotation.x);
-                }
-                if (obj.rotation.y != 0) {
-                    u_world = m4.yRotate(u_world, obj.rotation.y);
-                }
-                if (obj.rotation.z != 0) {
-                    u_world = m4.zRotate(u_world, obj.rotation.z);
-                }
-            }
-
-            for (const {
-                    bufferInfo,
-                    material
-                } of obj.parts) {
-                // calls gl.bindBuffer, gl.enableVertexAttribArray, gl.vertexAttribPointer
-                webglUtils.setBuffersAndAttributes(this.gl, programInfo, bufferInfo);
-
-                // calls gl.uniform
-                webglUtils.setUniforms(programInfo, {
-                    u_world
-                }, material);
-
-                // calls gl.drawArrays or gl.drawElements
-                webglUtils.drawBufferInfo(this.gl, bufferInfo);
-            }
+            computeObjWorld(obj);
         });
+
+        drawObjects(this.gl, objList, programInfo, cameraUniforms);
     }
+}
+
+function computeObjWorld(obj) {
+    let u_world = m4.identity();
+
+    // Handle object translation
+    if (obj.center && (obj.center.x != 0 || obj.center.y != 0 || obj.center.z != 0)) {
+        u_world = m4.translate(u_world, obj.center.x, obj.center.y, obj.center.z);
+    }
+
+    // Handle object rotation
+    //u_world = m4.xRotate(u_world, time);
+    //u_world = m4.yRotate(u_world, time);
+    //u_world = m4.zRotate(u_world, time);
+
+    if (obj.rotation) {
+        if (obj.rotation.x != 0) {
+            u_world = m4.xRotate(u_world, obj.rotation.x);
+        }
+        if (obj.rotation.y != 0) {
+            u_world = m4.yRotate(u_world, obj.rotation.y);
+        }
+        if (obj.rotation.z != 0) {
+            u_world = m4.zRotate(u_world, obj.rotation.z);
+        }
+    }
+
+    obj.uniforms.u_world = u_world;
+}
+
+function drawObjects(gl, objectsToDraw, programInfo, cameraUniforms) {
+    gl.useProgram(programInfo.program);
+    webglUtils.setUniforms(programInfo, cameraUniforms);
+
+    objectsToDraw.forEach(obj => {
+        for (const {
+                bufferInfo,
+                material
+            } of obj.parts) {
+            // calls gl.bindBuffer, gl.enableVertexAttribArray, gl.vertexAttribPointer
+            webglUtils.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+
+            // calls gl.uniform
+            webglUtils.setUniforms(programInfo, obj.uniforms, material);
+
+            // calls gl.drawArrays or gl.drawElements
+            webglUtils.drawBufferInfo(gl, bufferInfo);
+        }
+    });
 }
