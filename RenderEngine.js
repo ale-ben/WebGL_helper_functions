50847d789d1a16afc08756596853a9b38fa21ea7
Alessandro Benetton <aleben98@gmail.com>
2023-01-02 12:02:36 +0100

    [M3D-7-detect-click-on-object] Render auxiliary texture

--- RenderEngine.js
+++ RenderEngine.js
@@ -1,9 +1,35 @@
 export class RenderEngine {
     constructor(gl) {
         this.gl = gl;
-        webglUtils.resizeCanvasToDisplaySize(this.gl.canvas);
-        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
-        this.gl.enable(this.gl.DEPTH_TEST);
+		gl.enable(gl.CULL_FACE);
+        gl.enable(gl.DEPTH_TEST);
+
+		// Create a texture to render to
+		this.targetTexture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, this.targetTexture);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	
+		// create a depth renderbuffer
+		this.depthBuffer = gl.createRenderbuffer();
+		gl.bindRenderbuffer(gl.RENDERBUFFER, this.depthBuffer);
+	
+		// Create and bind the framebuffer
+		this.fb = gl.createFramebuffer();
+		gl.bindFramebuffer(gl.FRAMEBUFFER, this.fb);
+	
+		// attach the texture as the first color attachment
+		const attachmentPoint = gl.COLOR_ATTACHMENT0;
+		const level = 0;
+		gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, this.targetTexture, level);
+	
+		// make a depth buffer and the same size as the targetTexture
+		gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, this.depthBuffer);
+
+		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+		setFramebufferAttachmentSizes(gl, this.targetTexture, this.depthBuffer);
     }
 
     /**
@@ -12,13 +38,55 @@ export class RenderEngine {
      * @param {*} programInfo programInfo generated from webglUtils.createProgramInfo
      * @param {*} objList Array of objects to render. Each object can have a center object and a rotation object but must have a parts array.
      */
-    render(cameraUniforms, programInfo, objList) {
+    render(cameraUniforms, programInfo, objList, pickerProgramInfo) {
+
+		if (webglUtils.resizeCanvasToDisplaySize(this.gl.canvas)) {
+            // the canvas was resized, make the framebuffer attachments match
+            setFramebufferAttachmentSizes(this.gl, this.targetTexture, this.depthBuffer);
+        }
+
         objList.forEach(obj => {
             computeObjWorld(obj);
         });
 
+		// ------ Draw the objects to the texture --------
+
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.fb);
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+
+        this.gl.enable(this.gl.CULL_FACE);
+        this.gl.enable(this.gl.DEPTH_TEST);
+
+        // Clear the canvas AND the depth buffer.
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+
+        drawObjects(this.gl, objList, pickerProgramInfo, cameraUniforms);
+
+        // ------ Draw the objects to the canvas
+
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+
         drawObjects(this.gl, objList, programInfo, cameraUniforms);
     }
+
+
+}
+
+function setFramebufferAttachmentSizes(gl, targetTexture, depthBuffer) {
+	gl.bindTexture(gl.TEXTURE_2D, targetTexture);
+	// define size and format of level 0
+	const level = 0;
+	const internalFormat = gl.RGBA;
+	const border = 0;
+	const format = gl.RGBA;
+	const type = gl.UNSIGNED_BYTE;
+	const data = null;
+	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, gl.canvas.width, gl.canvas.height, border, format, type, data);
+
+	gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
+	gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, gl.canvas.width, gl.canvas.height);
 }
 
 function computeObjWorld(obj) {
